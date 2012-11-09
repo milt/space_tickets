cad7e87b08d45e6d13b5f332e9ed43942ecb585c
Milton Reder <milt@jhu.edu>
2012-11-09 14:07:35 -0500

    separated logic from game window

--- lib/space_tickets/game_window.rb
+++ lib/space_tickets/game_window.rb
@@ -5,45 +5,24 @@ module SpaceTickets
       super prefs.window_x, prefs.window_y, false
       self.caption = "Space Tickets"
 
-      @map = Map.new(self)
-
-      @background_image = Gosu::Image.new(self, "media/bk.png", true)
-
-      @player = Player.new(self)
-      @player.warp(prefs.window_x/2, prefs.window_x/2)
-
-      @ui = UserInterface.new(self)
+      @world = World.new(self)
     end
     
     def update
       if button_down? Gosu::KbLeft or button_down? Gosu::GpLeft then
-        @player.turn_left
+        @world.player.turn_left
       end
       if button_down? Gosu::KbRight or button_down? Gosu::GpRight then
-        @player.turn_right
+        @world.player.turn_right
       end
       if button_down? Gosu::KbUp or button_down? Gosu::GpButton0 then
-        @player.accelerate
+        @world.player.accelerate
       end
-      @player.move
-
-      unless @player.shifted.nil?
-        shift = @map.shift(@player.shifted)
-        if shift.nil?
-          @player.bounce
-        end
-        @ui.set_sector(@map.current_sector)
-      end
-      
-      @map.update
+      @world.update
     end
     
     def draw
-      @map.draw
-
-      @player.draw
-      @background_image.draw(0, 0, 0)
-      @ui.draw
+      @world.draw
     end
 
     def button_down(id)
---
+++ lib/space_tickets/world.rb
@@ -0,0 +1,36 @@
+module SpaceTickets
+  class World
+    attr_reader :player
+    def initialize(window)
+      @map = Map.new(window)
+
+      @background_image = Gosu::Image.new(window, "media/bk.png", true)
+
+      @player = Player.new(window)
+      @player.warp(window.width/2, window.height/2)
+
+      @ui = UserInterface.new(window)
+    end
+
+    def update
+      @player.move
+
+      unless @player.shifted.nil?
+        shift = @map.shift(@player.shifted)
+        if shift.nil?
+          @player.bounce
+        end
+        @ui.set_sector(@map.current_sector)
+      end
+      
+      @map.update
+    end
+
+    def draw
+      @map.draw
+      @player.draw
+      @background_image.draw(0, 0, 0)
+      @ui.draw
+    end
+  end
+end
